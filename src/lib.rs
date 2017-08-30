6d35bef30c58bcbd34eb7d052c4452ea7a9bac71
cssivision <cssivision@gmail.com>
2017-08-30 19:07:43 +0800

    remove deprecate boxed method

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
+extern crate futures;
 #[macro_use]
 extern crate log;
-extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate trust_dns;
 
 use std::io;
 use std::net::IpAddr;
-use std::net::{SocketAddr, Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::str;
 use std::time::Duration;
 
 use futures::future;
 use futures::Future;
-use tokio_io::io::{read_exact, write_all, Window, copy};
+use tokio_io::io::{copy, read_exact, write_all, Window};
 use tokio_io::AsyncRead;
 use tokio_core::net::TcpStream;
 use tokio_core::reactor::{Handle, Timeout};
@@ -30,56 +30,22 @@ pub struct Client {
 }
 
 impl Client {
-    /// This is the main entry point for starting a SOCKS proxy connection.
-    ///
-    /// This function is responsible for constructing the future which
-    /// represents the final result of the proxied connection. In this case
-    /// we're going to return an `IoFuture<T>`, an alias for
-    /// `Future<Item=T, Error=io::Error>`, which indicates how many bytes were
-    /// proxied on each half of the connection.
-    ///
-    /// The first part of the SOCKS protocol with a remote connection is for the
-    /// server to read one byte, indicating the version of the protocol. The
-    /// `read_exact` combinator is used here to entirely fill the specified
-    /// buffer, and we can use it to conveniently read off one byte here.
-    ///
-    /// Once we've got the version byte, we then delegate to the below
-    /// `serve_vX` methods depending on which version we found.
-    pub fn serve(self, conn: TcpStream)
-              -> Box<Future<Item=(u64, u64), Error=io::Error>> {
-        Box::new(read_exact(conn, [0u8]).and_then(|(conn, buf)| {
-            match buf[0] {
+    pub fn serve(self, conn: TcpStream) -> Box<Future<Item = (u64, u64), Error = io::Error>> {
+        Box::new(read_exact(conn, [0u8]).and_then(
+            |(conn, buf)| match buf[0] {
                 v5::VERSION => self.serve_v5(conn),
                 v4::VERSION => self.serve_v4(conn),
-
-                // If we hit an unknown version, we return a "terminal future"
-                // which represents that this future has immediately failed. In
-                // this case the type of the future is `io::Error`, so we use a
-                // helper function, `other`, to create an error quickly.
-                _ => future::err(other("unknown version")).boxed(),
-            }
-        }))
+                _ => Box::new(future::err(other("unknown version"))),
+            },
+        ))
     }
 
     /// Current SOCKSv4 is not implemented, but v5 below has more fun details!
-    fn serve_v4(self, _conn: TcpStream)
-                -> Box<Future<Item=(u64, u64), Error=io::Error>> {
-        future::err(other("unimplemented")).boxed()
+    fn serve_v4(self, _conn: TcpStream) -> Box<Future<Item = (u64, u64), Error = io::Error>> {
+        Box::new(future::err(other("unimplemented")))
     }
 
-    /// The meat of a SOCKSv5 handshake.
-    ///
-    /// This method will construct a future chain that will perform the entire
-    /// suite of handshakes, and at the end if we've successfully gotten that
-    /// far we'll initiate the proxying between the two sockets.
-    ///
-    /// As a side note, you'll notice a number of `.boxed()` annotations here to
-    /// box up intermediate futures. From a library perspective, this is not
-    /// necessary, but without them the compiler is pessimistically slow!
-    /// Essentially, the `.boxed()` annotations here improve compile times, but
-    /// are otherwise not necessary.
-    fn serve_v5(self, conn: TcpStream)
-                -> Box<Future<Item=(u64, u64), Error=io::Error>> {
+    fn serve_v5(self, conn: TcpStream) -> Box<Future<Item = (u64, u64), Error = io::Error>> {
         // First part of the SOCKSv5 protocol is to negotiate a number of
         // "methods". These methods can typically be used for various kinds of
         // proxy authentication and such, but for this server we only implement
@@ -94,23 +60,19 @@ impl Client {
         // another, but it also serves to simply have fallible computations,
         // such as checking whether the list of methods contains `METH_NO_AUTH`.
         let num_methods = read_exact(conn, [0u8]);
-        let authenticated = num_methods.and_then(|(conn, buf)| {
-            read_exact(conn, vec![0u8; buf[0] as usize])
-        }).and_then(|(conn, buf)| {
-            if buf.contains(&v5::METH_NO_AUTH) {
+        let authenticated = num_methods
+            .and_then(|(conn, buf)| read_exact(conn, vec![0u8; buf[0] as usize]))
+            .and_then(|(conn, buf)| if buf.contains(&v5::METH_NO_AUTH) {
                 Ok(conn)
             } else {
                 Err(other("no supported method given"))
-            }
-        }).boxed();
+            });
 
         // After we've concluded that one of the client's supported methods is
         // `METH_NO_AUTH`, we "ack" this to the client by sending back that
         // information. Here we make use of the `write_all` combinator which
         // works very similarly to the `read_exact` combinator.
-        let part1 = authenticated.and_then(|conn| {
-            write_all(conn, [v5::VERSION, v5::METH_NO_AUTH])
-        }).boxed();
+        let part1 = authenticated.and_then(|conn| write_all(conn, [v5::VERSION, v5::METH_NO_AUTH]));
 
         // Next up, we get a selected protocol version back from the client, as
         // well as a command indicating what they'd like to do. We just verify
@@ -120,23 +82,19 @@ impl Client {
         // As above, we're using `and_then` not only for chaining "blocking
         // computations", but also to perform fallible computations.
         let ack = part1.and_then(|(conn, _)| {
-            read_exact(conn, [0u8]).and_then(|(conn, buf)| {
-                if buf[0] == v5::VERSION {
-                    Ok(conn)
-                } else {
-                    Err(other("didn't confirm with v5 version"))
-                }
+            read_exact(conn, [0u8]).and_then(|(conn, buf)| if buf[0] == v5::VERSION {
+                Ok(conn)
+            } else {
+                Err(other("didn't confirm with v5 version"))
             })
-        }).boxed();
+        });
         let command = ack.and_then(|conn| {
-            read_exact(conn, [0u8]).and_then(|(conn, buf)| {
-                if buf[0] == v5::CMD_CONNECT {
-                    Ok(conn)
-                } else {
-                    Err(other("unsupported command"))
-                }
+            read_exact(conn, [0u8]).and_then(|(conn, buf)| if buf[0] == v5::CMD_CONNECT {
+                Ok(conn)
+            } else {
+                Err(other("unsupported command"))
             })
-        }).boxed();
+        });
 
         // After we've negotiated a command, there's one byte which is reserved
         // for future use, so we read it and discard it. The next part of the
@@ -153,33 +111,29 @@ impl Client {
             match buf[0] {
                 // For IPv4 addresses, we read the 4 bytes for the address as
                 // well as 2 bytes for the port.
-                v5::ATYP_IPV4 => {
-                    mybox(read_exact(c, [0u8; 6]).map(|(c, buf)| {
-                        let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
-                        let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
-                        let addr = SocketAddrV4::new(addr, port);
-                        (c, SocketAddr::V4(addr))
-                    }))
-                }
+                v5::ATYP_IPV4 => mybox(read_exact(c, [0u8; 6]).map(|(c, buf)| {
+                    let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
+                    let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
+                    let addr = SocketAddrV4::new(addr, port);
+                    (c, SocketAddr::V4(addr))
+                })),
 
                 // For IPv6 addresses there's 16 bytes of an address plus two
                 // bytes for a port, so we read that off and then keep going.
-                v5::ATYP_IPV6 => {
-                    mybox(read_exact(c, [0u8; 18]).map(|(conn, buf)| {
-                        let a = ((buf[0] as u16) << 8) | (buf[1] as u16);
-                        let b = ((buf[2] as u16) << 8) | (buf[3] as u16);
-                        let c = ((buf[4] as u16) << 8) | (buf[5] as u16);
-                        let d = ((buf[6] as u16) << 8) | (buf[7] as u16);
-                        let e = ((buf[8] as u16) << 8) | (buf[9] as u16);
-                        let f = ((buf[10] as u16) << 8) | (buf[11] as u16);
-                        let g = ((buf[12] as u16) << 8) | (buf[13] as u16);
-                        let h = ((buf[14] as u16) << 8) | (buf[15] as u16);
-                        let addr = Ipv6Addr::new(a, b, c, d, e, f, g, h);
-                        let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
-                        let addr = SocketAddrV6::new(addr, port, 0, 0);
-                        (conn, SocketAddr::V6(addr))
-                    }))
-                }
+                v5::ATYP_IPV6 => mybox(read_exact(c, [0u8; 18]).map(|(conn, buf)| {
+                    let a = ((buf[0] as u16) << 8) | (buf[1] as u16);
+                    let b = ((buf[2] as u16) << 8) | (buf[3] as u16);
+                    let c = ((buf[4] as u16) << 8) | (buf[5] as u16);
+                    let d = ((buf[6] as u16) << 8) | (buf[7] as u16);
+                    let e = ((buf[8] as u16) << 8) | (buf[9] as u16);
+                    let f = ((buf[10] as u16) << 8) | (buf[11] as u16);
+                    let g = ((buf[12] as u16) << 8) | (buf[13] as u16);
+                    let h = ((buf[14] as u16) << 8) | (buf[15] as u16);
+                    let addr = Ipv6Addr::new(a, b, c, d, e, f, g, h);
+                    let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
+                    let addr = SocketAddrV6::new(addr, port, 0, 0);
+                    (conn, SocketAddr::V6(addr))
+                })),
 
                 // The SOCKSv5 protocol not only supports proxying to specific
                 // IP addresses, but also arbitrary hostnames. This allows
@@ -206,24 +160,24 @@ impl Client {
                 // stub resolver, such as sorting of answers according to RFC
                 // 6724, more robust timeout handling, or resolving CNAME
                 // lookups.
-                v5::ATYP_DOMAIN => {
-                    mybox(read_exact(c, [0u8]).and_then(|(conn, buf)| {
-                        read_exact(conn, vec![0u8; buf[0] as usize + 2])
-                    }).and_then(move |(conn, buf)| {
-                        let (name, port) = match name_port(&buf) {
-                            Ok(UrlHost::Name(name, port)) => (name, port),
-                            Ok(UrlHost::Addr(addr)) => {
-                                return mybox(future::ok((conn, addr)))
-                            }
-                            Err(e) => return mybox(future::err(e)),
-                        };
+                v5::ATYP_DOMAIN => mybox(
+                    read_exact(c, [0u8])
+                        .and_then(|(conn, buf)| {
+                            read_exact(conn, vec![0u8; buf[0] as usize + 2])
+                        })
+                        .and_then(move |(conn, buf)| {
+                            let (name, port) = match name_port(&buf) {
+                                Ok(UrlHost::Name(name, port)) => (name, port),
+                                Ok(UrlHost::Addr(addr)) => return mybox(future::ok((conn, addr))),
+                                Err(e) => return mybox(future::err(e)),
+                            };
 
-                        let ipv4 = dns.query(name, DNSClass::IN, RecordType::A)
-                                      .map_err(|e| other(&format!("dns error: {}", e)))
-                                      .and_then(move |r| get_addr(r, port));
-                        mybox(ipv4.map(|addr| (conn, addr)))
-                    }))
-                }
+                            let ipv4 = dns.query(name, DNSClass::IN, RecordType::A)
+                                .map_err(|e| other(&format!("dns error: {}", e)))
+                                .and_then(move |r| get_addr(r, port));
+                            mybox(ipv4.map(|addr| (conn, addr)))
+                        }),
+                ),
 
                 n => {
                     let msg = format!("unknown ATYP received: {}", n);
@@ -285,8 +239,7 @@ impl Client {
             // standard.
             let addr = match c2.as_ref().map(|r| r.local_addr()) {
                 Ok(Ok(addr)) => addr,
-                Ok(Err(..)) |
-                Err(..) => addr,
+                Ok(Err(..)) | Err(..) => addr,
             };
             let pos = match addr {
                 SocketAddr::V4(ref a) => {
@@ -317,9 +270,7 @@ impl Client {
             // the connection.
             let mut w = Window::new(resp);
             w.set_end(pos + 2);
-            write_all(c1, w).and_then(|(c1, _)| {
-                c2.map(|c2| (c1, c2))
-            })
+            write_all(c1, w).and_then(|(c1, _)| c2.map(|c2| (c1, c2)))
         }));
 
         // Phew! If you've gotten this far, then we're now entirely done with
@@ -334,34 +285,34 @@ impl Client {
         // We then apply this timeout to the entire handshake all at once by
         // performing a `select` between the timeout and the handshake itself.
         let timeout = Timeout::new(Duration::new(10, 0), &self.handle).unwrap();
-        let pair = mybox(handshake_finish.map(Ok).select(timeout.map(Err)).then(|res| {
-            match res {
-                // The handshake finished before the timeout fired, so we
-                // drop the future representing the timeout, canceling the
-                // timeout, and then return the pair of connections the
-                // handshake resolved with.
-                Ok((Ok(pair), _timeout)) => Ok(pair),
+        let pair = mybox(handshake_finish.map(Ok).select(timeout.map(Err)).then(
+            |res| {
+                match res {
+                    // The handshake finished before the timeout fired, so we
+                    // drop the future representing the timeout, canceling the
+                    // timeout, and then return the pair of connections the
+                    // handshake resolved with.
+                    Ok((Ok(pair), _timeout)) => Ok(pair),
 
-                // The timeout fired before the handshake finished. In this
-                // case we drop the future representing the handshake, which
-                // cleans up the associated connection and all other
-                // resources.
-                //
-                // This automatically "cancels" any I/O associated with the
-                // handshake: reads, writes, TCP connects, etc. All of those
-                // I/O resources are owned by the future, so if we drop the
-                // future they're all released!
-                Ok((Err(()), _handshake)) => {
-                    Err(other("timeout during handshake"))
+                    // The timeout fired before the handshake finished. In this
+                    // case we drop the future representing the handshake, which
+                    // cleans up the associated connection and all other
+                    // resources.
+                    //
+                    // This automatically "cancels" any I/O associated with the
+                    // handshake: reads, writes, TCP connects, etc. All of those
+                    // I/O resources are owned by the future, so if we drop the
+                    // future they're all released!
+                    Ok((Err(()), _handshake)) => Err(other("timeout during handshake")),
+
+                    // One of the futures (handshake or timeout) hit an error
+                    // along the way. We're not entirely sure which at this
+                    // point, but in any case that shouldn't happen, so we just
+                    // keep propagating along the error.
+                    Err((e, _other)) => Err(e),
                 }
-
-                // One of the futures (handshake or timeout) hit an error
-                // along the way. We're not entirely sure which at this
-                // point, but in any case that shouldn't happen, so we just
-                // keep propagating along the error.
-                Err((e, _other)) => Err(e),
-            }
-        }));
+            },
+        ));
 
         // At this point we've *actually* finished the handshake. Not only have
         // we read/written all the relevant bytes, but we've also managed to
@@ -386,7 +337,7 @@ impl Client {
     }
 }
 
-fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item=F::Item, Error=F::Error>> {
+fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item = F::Item, Error = F::Error>> {
     Box::new(f)
 }
 
@@ -416,11 +367,12 @@ fn name_port(addr_buf: &[u8]) -> io::Result<UrlHost> {
     let port = ((addr_buf[pos] as u16) << 8) | (addr_buf[pos + 1] as u16);
 
     if let Ok(ip) = hostname.parse() {
-        return Ok(UrlHost::Addr(SocketAddr::new(ip, port)))
+        return Ok(UrlHost::Addr(SocketAddr::new(ip, port)));
     }
-    let name = try!(Name::parse(hostname, Some(&Name::root())).map_err(|e| {
-        io::Error::new(io::ErrorKind::Other, e.to_string())
-    }));
+    let name = try!(
+        Name::parse(hostname, Some(&Name::root()))
+            .map_err(|e| { io::Error::new(io::ErrorKind::Other, e.to_string()) })
+    );
     Ok(UrlHost::Name(name, port))
 }
 
@@ -429,13 +381,15 @@ fn get_addr(response: Message, port: u16) -> io::Result<SocketAddr> {
     if response.get_response_code() != ResponseCode::NoError {
         return Err(other("resolution failed"));
     }
-    let addr = response.get_answers().iter().filter_map(|ans| {
-        match *ans.get_rdata() {
+    let addr = response
+        .get_answers()
+        .iter()
+        .filter_map(|ans| match *ans.get_rdata() {
             RData::A(addr) => Some(IpAddr::V4(addr)),
             RData::AAAA(addr) => Some(IpAddr::V6(addr)),
             _ => None,
-        }
-    }).next();
+        })
+        .next();
 
     match addr {
         Some(addr) => Ok(SocketAddr::new(addr, port)),
